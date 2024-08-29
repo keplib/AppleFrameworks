66399e3ba391cf2f404bbed0b364629489044621
keplib <keplib@gmail.com>
2024-08-29 14:13:41 +0200

    Initial commit

---
+++ AppleFrameworks/FrameworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView.swift
+//  AppleFrameworks
+//
+//  Created by Balázs Képli on 29/08/2024.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
